7fee80c0b50d74fa8d1ca1aa6e3380767875c693
Veda8978122470 <155129300+Veda8978122470@users.noreply.github.com>
2023-12-29 14:31:57 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Even 
+{
+	public static void main (String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+	    int i;
+	    System.out.println("Enter a No");
+	    i=s.nextInt();
+	    if(i%2==0)
+		{
+			System.out.println("Even no");
+	    }
+		 else                                                                               
+		{                                         
+			  System.out.println("Odd no");
+	    }
+	}
+}	
