e9456271593ae8ff140588cd02022df573c31e81
Veda8978122470 <155129300+Veda8978122470@users.noreply.github.com>
2023-12-29 13:41:59 +0530

    Add files via upload

---
+++ biggest java.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Div
+{
+	public static void main (String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+	    int a,b,r;
+	    System.out.println("Enter Two Nos");
+	    a=s.nextInt();
+	    b=s.nextInt();
+	    if(a>b)
+	{
+			System.out.println(a+"is Biggest");
+	}
+	else
+		{	 	       		
+	System.out.println(b+"is Biggest");
+	}
+}
+
+
+	
+
+	
+
