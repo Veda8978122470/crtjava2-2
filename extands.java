5b191d841797dfe8777217a30d08ace6a15cd53f
Veda8978122470 <155129300+Veda8978122470@users.noreply.github.com>
2024-01-07 15:56:53 +0530

    Add files via upload

---
+++ extands.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class First
+{
+void m1()
+	{
+	System.out.println("m1 method in first class");
+	}
+	void m2()
+	{
+		System.out.println("m2 method in first class");
+	}
+}
+	class Second extends First
+	{
+	  void m3()
+	{
+		System.out.println("m3 method in second class");
+	}
+}
+class Test
+{
+	public static void main(String[] args)
+	{
+		Second s1=new second();
+		s1.m1();
+		s1.m2();
+		s1.m3();
+	}
+}
+
+
+
+
