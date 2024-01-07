2368a1b64d0e2a8ca5e4b386200d0dbc56bc8474
Veda8978122470 <155129300+Veda8978122470@users.noreply.github.com>
2024-01-07 16:04:12 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Data
+{
+	int a,b;
+	void get(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	void show()
+	{
+		System.out.println("a="+a+"b="+b);
+	}
+}
+class Addition extends Data
+{
+	void total()
+	{
+		System.out.println("Additional="(a+b));
+	}
+}
+class Multiply extends Data
+{
+	void product()
+	{
+		System.out.println("product="+(a*b));
+	}
+}
+class Single
+{
+	public static void main(String[] args)
+	{
+		Addition a1=new Addition();
+		a1.get(100,223);
+		a1.show();
+		a1.total();
+		Multiply m1=new Multiply();
+		m1.get(22,75);
+		m1.show();
+		m1.product();
+	}
+}
+
+
+	
+		
+	
+
