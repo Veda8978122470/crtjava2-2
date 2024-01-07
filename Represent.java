477468ed7a0be5eb45fca35d3238a545b1c7d527
Veda8978122470 <155129300+Veda8978122470@users.noreply.github.com>
2024-01-07 15:55:27 +0530

    Add files via upload

---
+++ Represent.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Represent
+{
+  int a,b;
+  void get()
+	{
+	  a=10;
+	  b=20;
+	}
+	  void show()
+	{
+	  System.out.println("a="+a+"b="+b);
+	}
+      public static void main(string[] args)
+	{
+		  Sample s1=new sample();
+		  Sample s2=new sample();
+		  s1.get();
+		  s2.get();
+		  s1.show();
+		  s2.show();
+	}
+}
+		 
